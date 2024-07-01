eb5661adec555aa2c2b1835e04b9a61d7cf53d98
akshat-kaushik <93491696+akshat-kaushik@users.noreply.github.com>
2024-07-02 02:02:54 +0530

    pass stage

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -11,14 +11,13 @@ func main() {
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
 		input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
-		inputarr:=strings.Split(input," ")
+		inputarr := strings.Split(strings.TrimSpace(input), " ")
 		command := inputarr[0]
 		switch command {
 		case "echo":
-			fmt.Fprint(os.Stdout,strings.Join(inputarr[1:]," "))
-
+			fmt.Fprint(os.Stdout, strings.Join(inputarr[1:], " ") + "\n")
 		default:
-			fmt.Fprint(os.Stdout, command+": command not found\n")
+			fmt.Fprint(os.Stdout, command + ": command not found\n")
 		}
 	}
 }
