4c3d5245112f71b53cc4b6e9753294184556c045
akshat-kaushik <93491696+akshat-kaushik@users.noreply.github.com>
2024-07-02 15:21:10 +0530

    pass stage

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,10 +4,12 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"slices"
 	"strings"
 )
 
 func main() {
+	validcmds:=[]string{"echo","exit","type"}
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
 		input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
@@ -18,6 +20,13 @@ func main() {
 			fmt.Fprint(os.Stdout, strings.Join(inputarr[1:], " ") + "\n")
 		case "exit":
 			os.Exit(0)
+		case "type":
+			if slices.Contains(validcmds,inputarr[1]){
+				fmt.Fprint(os.Stdout,command+" is a shell builtin")
+			}else{
+				fmt.Fprint(os.Stdout,command+" :not found")
+			}
+
 		default:
 			fmt.Fprint(os.Stdout, command + ": command not found\n")
 		}
