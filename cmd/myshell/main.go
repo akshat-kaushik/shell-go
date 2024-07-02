4fbc4db07a131f1ec1b49b205eaca1afa978aa46
akshat-kaushik <93491696+akshat-kaushik@users.noreply.github.com>
2024-07-02 20:29:55 +0530

    pass stage

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,12 +4,17 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"path/filepath"
 	"slices"
 	"strings"
 )
 
 func main() {
-	validcmds:=[]string{"echo","exit","type"}
+	validcmds := []string{"echo", "exit", "type", "ls"}
+	validLocalcmds:=[]string{"abcd"}
+	var path = os.Getenv("PATH")
+	var dirs = strings.Split(path, ":")
+
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
 		input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
@@ -17,18 +22,20 @@ func main() {
 		command := inputarr[0]
 		switch command {
 		case "echo":
-			fmt.Fprint(os.Stdout, strings.Join(inputarr[1:], " ") + "\n")
+			fmt.Fprint(os.Stdout, strings.Join(inputarr[1:], " ")+"\n")
 		case "exit":
 			os.Exit(0)
 		case "type":
-			if slices.Contains(validcmds,inputarr[1]){
-				fmt.Fprint(os.Stdout,inputarr[1]+" is a shell builtin\n")
+			if slices.Contains(validcmds, inputarr[1]) {
+				fmt.Fprint(os.Stdout, inputarr[1]+" is"+ filepath.Join(dirs[0],inputarr[1])+"\n")
+			} else if slices.Contains(validLocalcmds,inputarr[1]) {
+				fmt.Fprint(os.Stdout, inputarr[1]+" is"+ filepath.Join(dirs[1],inputarr[1])+"\n")
 			}else{
 				fmt.Fprint(os.Stdout,inputarr[1]+": not found\n")
 			}
 
 		default:
-			fmt.Fprint(os.Stdout, command + ": command not found\n")
+			fmt.Fprint(os.Stdout, command+": command not found\n")
 		}
 	}
 }
